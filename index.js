d65363c4c6c35d5221ef9a66f455c054b23e0ecc
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-24 12:48:52 +0500

    Fix dotenv config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
-const { error } = require('console');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -89,5 +89,3 @@ app.get('/boards', async (req, res) => {
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-createBoard();
